c1df8466812d22dd5fc35e6e6b656f547a9af2a9
Violettanik <Violettanik27@yandex.ru>
2025-06-07 11:11:10 +0300

    module5_02_task

---
+++ gopackages-layout/go.mod
@@ -0,0 +1,3 @@
+module gopackages-layout
+
+go 1.22.2
---
+++ gopackages-layout/internal/customer.go
@@ -0,0 +1,57 @@
+package internal
+
+import "errors"
+
+const DEFAULT_DISCOUNT = 30
+
+type Customer struct {
+    Name         string
+    Age          int
+    Balance      int
+    Debt         int
+    discount     bool
+    CalcDiscount func() (int, error)
+}
+
+// CalcPrice calculates the final price with discount
+func (customer *Customer) CalcPrice(price int) (int, error) {
+    if customer.CalcDiscount == nil {
+        return price, nil
+    }
+
+    discount, err := customer.CalcDiscount()
+    if err != nil {
+        return 0, err
+    }
+
+    finalPrice := price - (price * discount / 100)
+    if finalPrice < 0 {
+        finalPrice = 0
+    }
+
+    return finalPrice, nil
+}
+
+// NewCustomer creates a new Customer with default CalcDiscount function
+func NewCustomer(name string, age, balance, debt int, discount bool) Customer {
+    cust := Customer{
+        Name:     name,
+        Age:      age,
+        Balance:  balance,
+        Debt:     debt,
+        discount: discount,
+    }
+
+    cust.CalcDiscount = func() (int, error) {
+        if !cust.discount {
+            return 0, errors.New("discount not available")
+        }
+        result := DEFAULT_DISCOUNT - cust.Debt
+        if result < 0 {
+            return 0, nil
+        }
+        return result, nil
+    }
+
+    return cust
+}
